4f6ea4621ebbef1367af6b7ea46a27be0fa46c5b
Paul V <vial.paul@gmail.com>
2020-01-29 22:32:17 -0800

    Add complete folder structure from Cookiecutter Data Science

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='scrape analyse ze and predict mountain conditions',
+    author='Paul Vial',
+    license='',
+)
